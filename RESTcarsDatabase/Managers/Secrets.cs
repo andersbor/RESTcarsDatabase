99e45cc151885e56e66198664c148f9dee512524
Anders Børjesson <anbo@zealand.dk>
2021-02-24 21:55:38 +0100

    Add project files.

---
+++ RESTcarsDatabase/Managers/Secrets.cs
@@ -0,0 +1,8 @@
+﻿namespace RESTcarsDatabase.Managers
+{
+    public class Secrets
+    {
+        public const string ConnectionString =
+            "Server=tcp:anbo-databaseserver.database.windows.net,1433;Initial Catalog=anbobase;Persist Security Info=False;User ID=anbo;Password=Hemmelig14;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
