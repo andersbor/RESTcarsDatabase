df3721fc5caa64af590ea400c2756d54b83045e9
Anders Børjesson <anbo@zealand.dk>
2021-02-28 16:11:58 +0100

    entity framework + SqlClient

--- RESTcarsDatabase/Managers/Secrets.cs
+++ RESTcarsDatabase/Managers/Secrets.cs
@@ -2,7 +2,6 @@
 {
     public class Secrets
     {
-  
         public const string ConnectionString =
            "Server=tcp:anbo-databaseserver.database.windows.net,1433;Initial Catalog=anbobase;Persist Security Info=False;User ID=anbo;Password=Hemmelig14;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
