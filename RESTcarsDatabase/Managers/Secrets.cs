39bfb15bf9e0e7da044b77c7535348ec13ca2158
Anders Børjesson <anbo@easj.dk>
2021-02-28 15:54:19 +0100

    Update Secrets.cs

--- RESTcarsDatabase/Managers/Secrets.cs
+++ RESTcarsDatabase/Managers/Secrets.cs
@@ -2,7 +2,7 @@
 {
     public class Secrets
     {
-        public const string ConnectionString =
-            "Server=tcp:anbo-databaseserver.database.windows.net,1433;Initial Catalog=anbobase;Persist Security Info=False;User ID=anbo;Password=Hemmelig14;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string ConnectionString = "fill in your own connection string";
+           
     }
 }
